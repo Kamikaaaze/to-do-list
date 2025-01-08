99b131acfe740f496fd9d5d2ef852059cb4e3f5c
aswin us <20br13512@rit.ac.in>
2025-01-08 21:47:44 +0530

    add,delete function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState("");
 
   return (
-   <div className='mainDiv'>
-    <input className='typeAny' placeholder='Type here...' type='text'/>
-    <div className='box'>
-      <p className='title'>To Dos..</p>
-      <div className='toDoSet'>
-      <input id='todo' type='checkbox' className='check'/>
-      <label for='check' className='toDo'>Workout from 5pm to 6pm </label>
-      <i className="fa-solid fa-xmark"></i>
+    <div className="mainDiv">
+      <input
+        className="typeAny"
+        placeholder="Type here..."
+        type="text"
+        onChange={(e) => setToDo(e.target.value)}
+        value={toDo}
+      />
+      <i
+        onClick={() => {
+          if (toDo.trim()) {
+            setToDos([...toDos, toDo]);
+            setToDo(""); 
+          }
+        }}
+        className="fa-solid fa-plus"
+      ></i>
+      <div className="box">
+        <p className="title">To Dos..</p>
+
+      
+        {toDos.map((value, index) => {
+          return (
+            <div className="toDoSet" key={index}>
+              <input id={`todo-${index}`} type="checkbox" className="check" />
+              <label htmlFor={`todo-${index}`} className="toDo">
+                {value}
+              </label>
+              <i
+                className="fa-solid fa-xmark"
+                onClick={() => {
+                  setToDos(toDos.filter((_, i) => i !== index)); 
+                }}
+              ></i>
+            </div>
+          );
+        })}
       </div>
-    
     </div>
-   </div>
-  )
+  );
 }
 
-export default App
+export default App;
