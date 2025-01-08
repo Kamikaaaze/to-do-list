bbd77a4c5d3245ddc88da21e573c103b587ffa0d
aswin us <20br13512@rit.ac.in>
2025-01-08 18:56:45 +0530

    added input and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ function App() {
 
   return (
    <div className='mainDiv'>
+    <input className='typeAny' placeholder='Type here...' type='text'/>
     <div className='box'>
       <p className='title'>To Dos..</p>
       <div className='toDoSet'>
-      <input type='checkbox' className='check'/>
-      <p className='toDo'>Workout from 5pm to 6pm</p>
+      <input id='todo' type='checkbox' className='check'/>
+      <label for='check' className='toDo'>Workout from 5pm to 6pm </label>
+      <i className="fa-solid fa-xmark"></i>
       </div>
     
     </div>
